5bd3435421e8029c0d379c2688ff4ceefa1af1fa
Nikita <32309387+TepNik@users.noreply.github.com>
2020-10-01 14:26:25 +0300

    Update ERC223.sol

--- contracts/ERC223/ERC223.sol
+++ contracts/ERC223/ERC223.sol
@@ -88,4 +88,45 @@ contract ERC223Token is IERC223, Ownable {
     function balanceOf(address _owner) public view returns (uint balance) {
         return balances[_owner];
     }
+    
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+    
+    constructor () public {
+        _name = "Smart Advertising Transaction Token";
+        _symbol = "SATT";
+        _decimals = 18;
+    }
+
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @dev Returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5,05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei.
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
 }
